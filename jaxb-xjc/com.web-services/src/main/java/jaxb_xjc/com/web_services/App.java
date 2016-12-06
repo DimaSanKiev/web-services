4a1ed8ed9a8b04661dc183cabb5acd4437d590bd
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-12-06 23:36:09 +0200

    add jsxb-xjc project

---
+++ jaxb-xjc/com.web-services/src/main/java/jaxb_xjc/com/web_services/App.java
@@ -0,0 +1,13 @@
+package jaxb_xjc.com.web_services;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
